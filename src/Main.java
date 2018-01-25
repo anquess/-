83117d804cea068dd8395333dd3b8150fa9be28e
asci02 <higashiyama-k105r@ascsys.onmicrosoft.com>
2018-01-25 10:09:27 +0900

    校歌を表示するMainメソッド【分割対象課題】とそれを保証する単体テストを作成

---
+++ src/Main.java
@@ -0,0 +1,119 @@
+public class Main {
+	public static void main(String[] args)throws Exception{
+		for (int i = 0; i < 3; i++) {
+			singLine1(i);
+			singLine2(i);
+			singLine3(i);
+			singLine4(i);
+			singLine5(i);
+			singLine6(i);
+		}
+	}
+
+	public static void singLine1(int i) {
+		String lyrics = "エラー";
+		switch(i){
+		case 0:
+			lyrics = "青き海原　はるかに望み";
+			break;
+		case 1:
+			lyrics = "緑の大地に　そよ風かおり";
+			break;
+		case 2:
+			lyrics = "真白き雲間に　新たな道を";
+			break;
+		}
+		System.out.println(lyrics);
+	}
+
+	public static void singLine2(int i) {
+		String lyrics = "エラー";
+		switch(i){
+		case 0:
+			lyrics = "きらめく翼よ";
+			break;
+		case 1:
+			lyrics = "豊けき学び舎";
+			break;
+		case 2:
+			lyrics = "広がる世界よ";
+			break;
+		}
+		lyrics = lyrics + singAddSame(2);
+		System.out.println(lyrics);
+	}
+
+	public static void singLine3(int i) {
+		String lyrics = "エラー";
+		switch(i){
+		case 0:
+			lyrics = "雲つらぬく轟き　見守る";
+			break;
+		case 1:
+			lyrics = "声かけあい励まし　青春";
+			break;
+		case 2:
+			lyrics = "音速超えゆく時代の　若人";
+			break;
+		}
+		lyrics = lyrics + singAddSame(3);
+		System.out.println(lyrics);
+	}
+
+	public static void singLine4(int i) {
+		String lyrics = "エラー";
+		switch(i){
+		case 0:
+			lyrics = "夢はてしなき";
+			break;
+		case 1:
+			lyrics = "歌こだまする";
+			break;
+		case 2:
+			lyrics = "星めぐり逢う";
+			break;
+		}
+		lyrics = lyrics + singAddSame(4);
+		System.out.println(lyrics);
+	}
+
+	public static void singLine5(int i) {
+		String lyrics = "エラー";
+		switch(i){
+		case 0:
+			lyrics = "翔ける";
+			break;
+		case 1:
+			lyrics = "学ぶ";
+			break;
+		case 2:
+			lyrics = "生きる";
+			break;
+		}
+		lyrics = lyrics + singAddSame(5);
+		System.out.println(lyrics);
+	}
+
+	public static void singLine6(int i) {
+		System.out.println(singAddSame(6));
+	}
+
+	private static String singAddSame(int i) {
+		switch (i) {
+		case 2:
+			return "　今幸あれ";
+		case 3:
+			return "われら";
+		case 4:
+			return "　大空に";
+		case 5:
+			return "歓び　ともに";
+		case 6:
+			return "航空保安大学校\n";
+
+		}
+		return null;
+	}
+
+
+}
---
+++ tst/MainTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void mainメソッドを実行して校歌が表示されるtest() throws Exception{
+		// Arrange
+		final List<String> actual = new ArrayList<String>();
+		PrintStream fakePrintStream = new PrintStream("ファイル名はここではどうでもいい") {
+			@Override
+			public void println(String str) {
+				actual.add(str);
+				}
+		};
+
+		// Act
+		System.setOut(fakePrintStream);
+		Main.main(null);
+
+		// Assert
+		List<String> expected = Arrays.asList("青き海原　はるかに望み", "きらめく翼よ　今幸あれ",
+				"雲つらぬく轟き　見守るわれら", "夢はてしなき　大空に", "翔ける歓び　ともに",
+				"航空保安大学校\n", "緑の大地に　そよ風かおり", "豊けき学び舎　今幸あれ",
+				"声かけあい励まし　青春われら", "歌こだまする　大空に", "学ぶ歓び　ともに",
+				"航空保安大学校\n", "真白き雲間に　新たな道を", "広がる世界よ　今幸あれ",
+				"音速超えゆく時代の　若人われら", "星めぐり逢う　大空に", "生きる歓び　ともに",
+				"航空保安大学校\n");
+        assertEquals(expected, actual);
+
+	}
+
+}
